0771770ec501c650fba894d6f3e867282c949b78
Rhuan Levy <rhuan_os@hotmail.com>
2023-09-26 15:16:52 -0300

    First commit

---
+++ src/mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: 1,
+    nome: 'Rhuan',
+  },
+  {
+    id: 2,
+    nome: 'Igor',
+  },
+  {
+    id: 3,
+    nome: 'Ninho',
+  },
+  {
+    id: 4,
+    nome: 'Roberto',
+  },
+]
